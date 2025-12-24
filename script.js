828c009db8c48fbf4f02cdc268b6db60dee616d7
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-12-24 18:14:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //your code here
 
+let todos=[];
+
+const addbtn=document.getElementById("addTodoBtn");
+const ol=document.getElementById("todoList");
+
+addbtn.addEventListener('click',()=>{
+	 const input = document.getElementById("newTodoInput").value.trim();
+	if(!input){
+		alert("Todo should be filled");
+		return;
+	}
+	todos.push(input);
+	 document.getElementById("newTodoInput").value = ""; 
+	renderlist();
+})
+
+function  renderlist() {
+	ol.innerHTML = ""; 
+	todos.forEach(todo=>{
+		const li=document.createElement('li');
+		li.textContent=todo;
+		ol.appendChild(li);
+	})
+}
+
+
